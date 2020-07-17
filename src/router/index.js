aa2bb5d94752f50d27a0b2f1fb7c2730f8273687
Temirlan <kyrmanbaev67@mail.ru>
2020-07-17 19:43:07 +0500

    added new layout for burger

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,16 @@
     Vue.use(VueRouter);
 
     // Router components
+
+
+    // burger
+    import Burger from '../components/burger/burger.vue';
+
+
+
+
+
+    
     import AuthPage from '../components/auth/AuthPage.vue';
     import Login from '../components/auth/components/login.vue';
     import ForgotPassword from '../components/auth/components/forgotPassword.vue';
@@ -28,6 +38,7 @@
                 component: AuthPage,
                 meta: { guest: true },
                 children: [
+                    // { path: 'burger', component: Burger},
                     { path: '', component: Login },
                     { path: 'login', component: Login },
                     { path: 'forgotPassword', component: ForgotPassword },
@@ -35,7 +46,14 @@
                     { path: 'reg_confirm', component: Reg_confirm },
                     { path: 'check', component: Check }
                 ]
-            }, 
+            },
+        
+        {
+            path: '/burger',
+            component: Burger,
+            meta: { guest: true },
+
+        }, 
             {
                 path: '/user',
                 component: UserPage,
